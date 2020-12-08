6f7e7add9c1ebff5c382b91a421e4db81e9e22d1
Taeng <dyek72731@gmail.com>
2020-12-09 00:29:31 +0900

    3rd working

--- 00-react-env/src/App.js
+++ 00-react-env/src/App.js
@@ -1,34 +1,12 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import Tours from './components/tours'
-import Loader from './components/loader'
+import Review from './components/review'
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [tours, setTours] = useState([]);
-  const url = 'https://course-api.com/react-tours-project';
-
-  const getTours = async () => {
-    const res = await fetch(url);
-    const data = await res.json();
-    setTours(data);
-    setLoading(false);
-  }
-
-  useEffect(() => {
-    getTours();
-  }, [])
-
-  const handleClick = (idx) => {
-    const newTours = tours.filter(tour => tour.id !== idx);
-    setTours(newTours)
-  }
+  const [reviewIndex, setReviewIndex] = useState(0);
 
   return (
     <section className="App">
-      {loading && <Loader />}
-      {!loading && <h2>Our Tours</h2>}
-      <Tours tours={tours} handleClick={handleClick} />
     </section>
   );
 }

d97bbf0245308feb87fcc3e87272489439408fbd
Taeng <dyek72731@gmail.com>
2020-12-13 23:23:07 +0900

    13th working

--- 00-envi/README.md
+++ 00-envi/README.md
@@ -1,4 +1,4 @@
-# 08. Lorem Ipsum : Feedback
+# 13. Stripe Submenus : Feedback
 
 ## HTML/CSS
 
@@ -12,23 +12,4 @@
 App.js 에서 <Navbar /> 컴포넌트를 받았다. links, social 데이터는 <Navbar /> 에서 바로 import.
 
 ### 1. Navbar.js / useRef() Hooks
-useRef 는 특정 DOM을 참고할 때 사용되는 Hooks 이다. current에 최근 지정한 DOM을 저장한다.
 
-import React, { useRef } from 'react';
-
-function App() => {
-  // 1) colorChange 라는 useRef 변수를 생성
-  const colorChange = useRef(null);
-  // 2) 이벤트 핸들러에서, colorChange ref의 스타일 변경
-  const handleChange = () => {
-    colorChange.current.style = "background: red"
-  }
-
-  return (
-    <>
-    // 3) 대상 DOM의 ref엔 colorChange(useRef 변수) 지정
-      <div ref={colorChange} style={{background: 'blue'}}>
-      <button onClick={handleChange}>Ref</button>
-    <>
-  )
-}
--- 00-envi/src/App.js
+++ 00-envi/src/App.js
@@ -1,18 +1,12 @@
-import React, { useState } from 'react';
-import { BiMenu } from 'react-icons/bi'
-import Modal from './components/modal'
-import Side from './components/sidebar'
+import React from 'react';
+import Header from './components/header'
+import Main from './components/main'
 
 function App() {
-  const [showModal, setShowModal] = useState(false);
-  const [showSide, setShowSide] = useState(false);
-  
   return (
     <div className="App">
-      <button className="side-btn" onClick={() => setShowSide(true)}><BiMenu /></button>
-      <Side showSide={showSide} closeSide={() => setShowSide(false)}/>
-      <button className="modal-btn" onClick={() => setShowModal(true)}>Show Modal</button>
-      {showModal && <Modal closeModal={() => setShowModal(false)}/>}
+      <Header />
+      <Main />
     </div>
   );
 }
